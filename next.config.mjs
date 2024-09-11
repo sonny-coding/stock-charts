fd78a04182d32cc4f785a0256f0ecb76f0277b84
Son Dao <udemydaohsn@gmail.com>
2024-09-11 11:27:28 -0700

    update json file

--- next.config.mjs
+++ next.config.mjs
@@ -4,16 +4,6 @@ const nextConfig = {
     config.externals.push("@node-rs/argon2", "@node-rs/bcrypt");
     return config;
   },
-  reactStrictMode: true,
-  pageExtensions: [
-    "page.tsx",
-    "page.ts",
-    // FIXME: Next.js has a bug which does not resolve not-found.page.tsx correctly
-    // Instead, use `not-found.ts` as a workaround
-    // "ts" is required to resolve `not-found.ts`
-    // https://github.com/vercel/next.js/issues/65447
-    "ts",
-  ],
 };
 
 export default nextConfig;
--- src/app/not-found.tsx
+++
@@ -1,7 +0,0 @@
-// app/not-found.tsx
-
-export const dynamic = "force-dynamic";
-
-export default function NotFound() {
-  return <></>;
-}
